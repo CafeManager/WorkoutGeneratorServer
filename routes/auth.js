57929939e020e2aeaaba0a1ba0ef644bd2bb8177
CafeManager <kevinflores1997@yahoo.com>
2023-12-13 12:25:44 -0800

    fixed compiler error

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,11 @@ const jsonschema = require("jsonschema");
 const User = require("../models/user");
 const express = require("express");
 const router = new express.Router();
-const { createToken } = require("../helpers/tokens");
+// const { createToken } = require("../helpers/tokens");
 const { BadRequestError } = require("../expressError");
 
 router.post("/token", async function (req, res, next) {
     try {
-        
         const { username, password } = req.body;
         const user = await User.authenticate(username, password);
         const token = createToken(user);
